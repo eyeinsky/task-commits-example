e27654c384e2c7f82a0225319eae20a36f8aa446
eyeinsky <eyeinsky9@gmail.com>
2020-04-02 13:26:59 +0300

    Fix case when task not found

--- get_task_feature_branch.sh
+++ get_task_feature_branch.sh
@@ -3,7 +3,7 @@
 TASK_ID="$1"
 
 # where feature branch was merged
-MERGE_COMMIT=$(git log --oneline -1 -p ":/Merge branch.*#$TASK_ID" --format=format:"%H")
+MERGE_COMMIT=$(git log --oneline -1 -p ":/Merge branch.*#$TASK_ID" --format=format:"%H" 2>/dev/null || echo)
 
 if [[ "$MERGE_COMMIT" != "" ]]; then
     echo MERGE_COMMIT \"$MERGE_COMMIT\"
@@ -13,9 +13,15 @@ if [[ "$MERGE_COMMIT" != "" ]]; then
 
     # log feature branch commits
     git log $PARENT $MERGE_COMMIT
-else
-    SINGLE_COMMIT=$(git log --oneline -1 ":/#$TASK_ID" --format=format:"%H")
-    echo SINGLE_COMMIT \"$SINGLE_COMMIT\"
-
-    git log -1 $SINGLE_COMMIT
+    exit
 fi
+
+SINGLE_COMMIT=$(git log --oneline -1 ":/#$TASK_ID" --format=format:"%H" 2>/dev/null || echo)
+
+if [[ $SINGLE_COMMIT != "" ]]; then
+   echo SINGLE_COMMIT \"$SINGLE_COMMIT\"
+   git log -1 $SINGLE_COMMIT
+   exit
+fi
+
+echo Task not found: \#$TASK_ID
